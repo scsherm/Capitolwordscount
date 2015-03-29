ffbd4c79ab24c3bf4f14c62ae4e8ff5f87c12ec5
scsherm <scsherm@gmail.com>
2015-03-28 22:05:18 -0600

    commit 1

---
+++ Bfrankwords.py
@@ -0,0 +1,23 @@
+import requests
+import csv
+import pprint
+from nltk.corpus import stopwords
+
+query_params = { 'apikey': 'f6ab5f2e4f69444b9f2c0a44d9a5223d',
+		   		 'entity_type': 'legislator',
+		   		 'entity_value': 'f000339',
+                         'sort': 'count desc'
+                         }
+
+endpoint = 'http://capitolwords.org/api/phrases.json'
+response = requests.get(endpoint, params=query_params).json()
+
+with open("BFrank_words.csv", "w") as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerow(['word', 'count'])
+
+	for n in response:
+         if n['ngram'] not in stopwords.words('english'):
+             word = n['ngram']
+             count = n['count']
+             writer.writerow([word, count])
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Capitolwordscount
 For looking at top words spoken by legislators
+
+## Uses the Sunlight Foundation Capitol Words API to create a list of the top spoken words by legislators while excluding any stopwords  
